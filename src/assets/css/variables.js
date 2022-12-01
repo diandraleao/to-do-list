1c3b2b9ff982d8afd61335c1fc783db40c96eca0
Diandra <leao.diandra@gmail.com>
2022-12-01 13:21:28 -0300

    add style vars

---
+++ src/assets/css/variables.js
@@ -0,0 +1,31 @@
+export default {
+
+  color: {
+    product: {
+      purple: "#8284FA",
+      purpleDark: "#5E60CE",
+      blue: "#4EA8DE",
+      blueDark: "#1E6F9F"
+    },
+    base: {
+      gray700: "#0D0D0D",
+      gray600: "#1A1A1A",
+      gray500: "#262626",
+      gray400: "#333333",
+      gray300: "#808080",
+      gray200: "#D9D9D9",
+      gray100: "#F2F2F2",
+      danger: "#E25858"
+    }
+  },
+
+  fontSize: {
+    sm: "12px",
+    md: "14px",
+    lg: "16px",
+    xl: "18px"
+  },
+
+  lineHeight: "140%"
+
+}
