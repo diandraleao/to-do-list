02bd0ddcc7e741ac512934039472306b1808ac91
Diandra <leao.diandra@gmail.com>
2022-12-07 19:19:53 -0300

    refact: all the component

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,26 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { 
-  Box,
-  LabelId,
-  LabelTitle,
-  Button
-} from './Style'
+import * as S from './Style'
+import { LIST_TASK } from '../../utils/strings/pt-BR'
+import Trash from '../../assets/img/trash.svg'
 
-function Task(props) {
+function Task( { tasks, removeTask, completeTask } ) {
+
+  const [checkbox, setCheckbox] = useState(false)
+  const [isCompleted, setCompleted] = useState(false);
+
+  const completeTaskHandler = (e) => {
+    setCheckbox()
+    setCompleted(!isCompleted);
+    console.log('completeTaskHandler')
+    completeTask()
+  }
+
   return (
-    <Box>
-      <input type="checkbox"
-        id={'task-'+props.usuario.id}
-        name={'task-'+props.usuario.id}
-        value={'task-'+props.usuario.id}
-        checked={props.usuario.completed}
-        onChange={props.completeTask}
-      />
-      <LabelId>#{props.usuario.id}</LabelId>
-      <LabelTitle>{props.usuario.title}</LabelTitle>
-      <Button onClick={props.removeTask}>&times;</Button>
-    </Box>
+    <>
+      <S.StatusHeading>
+        <S.AllTasksTitle>{LIST_TASK.headingCompleted}
+          <S.Counter>3</S.Counter>
+        </S.AllTasksTitle>
+        <S.CompletedTasksTitle>{LIST_TASK.headingCompletedTasks}
+          <S.Counter>1</S.Counter>
+        </S.CompletedTasksTitle>
+      </S.StatusHeading>
+      {tasks.map((task, index) => (
+        console.log('index: ' + index),
+        <S.Box key={'box-'+index}>
+          <div>
+            <input type="checkbox"
+            id={'task-' + task.id}
+            name={'task-' + task.id}
+            value={task.completed}
+            defaultChecked={task.completed}
+            onChange={(e) => (console.log(e))} />
+              <S.LabelTitle className={isCompleted ? 'completed' : ''}>
+                {task.title}
+              </S.LabelTitle>
+              <S.Button onClick={removeTask}>
+                <img src={Trash} alt='icone lixeira' />
+              </S.Button>
+          </div>
+        </S.Box>
+      ))}
+    </>
   )
 }
 
