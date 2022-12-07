e5f0cf7608dcf85d8d4703932214d854574604ea
Diandra <leao.diandra@gmail.com>
2022-12-07 19:16:54 -0300

    refact: submit function, imports & jsx

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,56 +1,54 @@
 import React, { useState } from 'react'
 
-import { 
-  Task,
-  Linha,
-  Input,
-  Button
-} from './Style'
+import * as S from './Style'
+import Plus from '../../assets/img/plus.svg'
 import { ADD_TASK } from '../../utils/strings/pt-BR'
+// import randomIntFromInterval from '../../utils/handlers/Numbers'
 
 function AddTask(props) {
   
-  const [nome, setNome] = useState('')
-  const [sobrenome, setSobrenome] = useState('')
-  const [email, setEmail] = useState('')
+  const [description, setDescription] = useState('')
+  let arrayIds = []
+  let currentId = 1
 
   const onSubmitHandler = event => {
     event.preventDefault()
 
-    const usuario = { nome, sobrenome, email }
+    arrayIds.push(currentId)
+    const task = {id: currentId, title: description, completed: false}
+    currentId += 1
 
-    fetch('https://reqres.in/api/users', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(usuario)
-    })
-      .then(resposta => resposta.json())
-      .then(dados => {
-        setNome('')
-        setSobrenome('')
-        setEmail('')
-        props.adicionarUsuario(dados)
-      })
+    if (!description) {
+      console.log('Digite uma task')
+      return;
+    }
+
+    props.addTask(task)
+    console.log('task submitted')
+    setDescription('');
   }
 
   return (
-    <Task>
-      <form onSubmit={onSubmitHandler}>
-        <Linha>
-          <Input
-            type="text"
-            name="nome"
-            value={nome}
-            placeholder={ADD_TASK.labelTask}
-            onChange={event => setNome(event.target.value)}
-            required />
-        </Linha>
-        
-        <Button type="submit">
-          {ADD_TASK.labelButton}
-        </Button>
-      </form>
-    </Task>
+    <S.Task>
+      <S.Container>
+        <form onSubmit={onSubmitHandler}>
+            <S.Title>{ADD_TASK.title}</S.Title>
+            <div>
+              <S.Input
+                type="text"
+                name="description"
+                value={description}
+                placeholder={ADD_TASK.labelTask}
+                onChange={event => setDescription(event.target.value)}
+                required />
+              <S.Button type="submit">
+                {ADD_TASK.labelButton}
+                <img src={Plus} alt='icone de adicionar' />
+              </S.Button>
+            </div>
+        </form>
+      </S.Container>
+    </S.Task>
   )
 }
 
